1401987fd9f9572410385e4ff1c4193bfb6aa084
Nikita Lalwani <nktllwn@gmail.com>
2025-03-12 13:38:37 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'firebasestorage.googleapis.com',
-      port: '',
-      pathname: '/**',
-    },
-  ],
-},
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5000/api/:path*" // Local dev server
+            : "/api/:path*", // Use serverless API routes in production
+      },
+    ];
+  },
 };
 
 export default nextConfig;
